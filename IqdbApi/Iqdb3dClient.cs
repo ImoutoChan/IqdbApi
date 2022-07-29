91720e69489aa9aae441882369e0d61b31f2a513
Alex1911 <tjxx_jgd@163.com>
2022-07-29 22:39:57 +0800

    Repair 3d SearchFile (#9)
    * Repair SearchFile
    
    * Add Winform Manual Tests

--- IqdbApi/Iqdb3dClient.cs
+++ IqdbApi/Iqdb3dClient.cs
@@ -1,3 +1,4 @@
+using System.IO;
 using System.Net.Http;
 
 namespace IqdbApi
@@ -11,6 +12,19 @@ namespace IqdbApi
             int waitMilliseconds = 5100,
             bool useHttps = true) : base(httpMessageHandler, waitMilliseconds, useHttps)
         {
+        }
+
+        protected override MultipartFormDataContent GetFromDataContent(Stream fileStream)
+        {
+            var form = new MultipartFormDataContent();
+            form.Headers.Add("Origin", "http://3d.iqdb.org");
+
+            form.Add(new StringContent("38333838363038"), "MAX_FILE_SIZE");
+
+            form.Add(new StreamContent(fileStream), "file", "image.jpg");
+            form.Add(new StringContent(string.Empty), "url");
+
+            return form;
         }
     }
 }
